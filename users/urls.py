1d1c7b12a9d5f25735b0ad448ceb45a26bdaf0bc
Alash Shrestha <alashsth2019@gmail.com>
2024-02-09 14:12:00 +0545

    Add form and csrf token

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from users.views import home
+from users.views import login_page
 
+app_name = "users"
 urlpatterns = [
-    path('', home),
+    path('', login_page, name='login'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
+def login_page(request):
     return render(request, 'users/login.html')
