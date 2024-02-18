e11715c82ef1a009cd1f7b2a37ef428f1d6b1968
Alash Shrestha <alashsth2019@gmail.com>
2024-02-18 19:04:48 +0545

    Products and Category Model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+        
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='product_images/')
+    description = models.TextField()
+    price = models.FloatField(max_length=10)
+    quantity = models.IntegerField(max_length=10)
+    stock = models.BooleanField(default=False)
+    discount = models.FloatField(max_length=10)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
