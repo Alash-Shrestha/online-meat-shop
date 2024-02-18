eda133a211f40f2ad24e4640de0f496150d080a0
Alash Shrestha <alashsth2019@gmail.com>
2024-02-18 21:30:16 +0545

    Update urls

--- meat_shop/urls.py
+++ meat_shop/urls.py
@@ -19,6 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('users.urls')),
-    path('home/', include('products.urls')),
+    path('login/', include('users.urls')),
+    path('', include('products.urls')),
 ]
