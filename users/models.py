f507299272a7bd9fa8e3ffebba5edb875a78d9b3
Alash Shrestha <alashsth2019@gmail.com>
2024-02-18 19:03:14 +0545

    Users Model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='user_images/', null=True, blank=True)
+    email = models.EmailField(max_length=200)
+    password = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15, blank=True)
+    address = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_customer = models.BooleanField(default=False)
+    is_vendor = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name
