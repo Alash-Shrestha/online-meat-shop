a08bcd57925046bc4d0e0110ba568d5fef876834
Alash Shrestha <alashsth2019@gmail.com>
2024-03-22 16:02:32 +0545

    Add Models in admin site

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from products.models import Product, Category
+from products.models import *
 
 # Register your models here.
-admin.site.register(Product)
 admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
