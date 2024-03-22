d029ecdf4be61f9eb68d15f454f67f3816c837bb
Alash Shrestha <alashsth2019@gmail.com>
2024-03-22 19:00:03 +0545

    Removed name and email field, and added phone field.

--- users/models.py
+++ users/models.py
@@ -4,8 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
-    name = models.CharField(max_length=200, null=True)
-    email = models.EmailField(max_length=200, null=True)
+    phone = models.CharField(max_length=20, null=True)
     
     def __str__(self):
-        return self.name
+        return self.user.username
