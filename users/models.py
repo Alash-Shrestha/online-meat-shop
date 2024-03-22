c20ff7a52c262599dbf26fdab765d411be0dc142
Alash Shrestha <alashsth2019@gmail.com>
2024-03-22 16:11:30 +0545

    Created Customer Model

--- users/models.py
+++ users/models.py
@@ -1,18 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
-    name = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='user_images/', null=True, blank=True)
-    email = models.EmailField(max_length=200)
-    password = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=15, blank=True)
-    address = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    is_admin = models.BooleanField(default=False)
-    is_customer = models.BooleanField(default=False)
-    is_vendor = models.BooleanField(default=False)
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(max_length=200, null=True)
     
     def __str__(self):
         return self.name
