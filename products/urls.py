d74de2e84cc34c58626bbc0796c3f74047b52e32
Alash Shrestha <alashsth2019@gmail.com>
2024-03-22 16:04:09 +0545

    Add product path

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from products.views import home
+from products.views import home, product
 
 app_name = "products"
 urlpatterns = [
     path('', home, name='home'),
+    path('product/<str:name>/', product, name='product'),
 ]
