fe4e733d309962bb80e4bd94b967126cc8dd9cd8
Alash Shrestha <alashsth2019@gmail.com>
2024-03-22 16:03:02 +0545

    Product Models

--- products/models.py
+++ products/models.py
@@ -1,24 +1,99 @@
 from django.db import models
+from users.models import Customer
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
-        
+
     def __str__(self):
         return self.name
-    
+
+
+class UnitType(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='product_images/')
-    description = models.TextField()
-    price = models.FloatField(max_length=10)
-    quantity = models.IntegerField()
-    stock = models.BooleanField(default=False)
-    discount = models.FloatField(max_length=10)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    price = models.FloatField(max_length=6)
+    # price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    unit_type = models.ForeignKey(
+        UnitType, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    description = models.CharField(max_length=250, default="", blank=True, null=True)
+    image = models.ImageField(upload_to="product/", null=True, blank=True)
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.FloatField(max_length=6)
 
     def __str__(self):
         return self.name
 
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ""
+        return url
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.SET_NULL, blank=True, null=True
+    )
+    image = models.ImageField(upload_to="product_image/", null=True, blank=True)
+
+    def __str__(self):
+        return self.product
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ""
+        return url
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.SET_NULL, blank=True, null=True
+    )
+    date_ordered = models.DateField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.product
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.SET_NULL, blank=True, null=True
+    )
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.IntegerField(default=0, blank=True, null=True)
+    date_added = models.DateField(auto_now_add=True)
+
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.SET_NULL, blank=True, null=True
+    )
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    state = models.CharField(max_length=200, null=True)
+    zipcode = models.CharField(max_length=200, null=True)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
