3f7f072b97102b12c8c0f238b8ebb26ad10d5378
Alash Shrestha <alashsth2019@gmail.com>
2024-03-22 15:58:50 +0545

    Add users path, Included media url and Remove login url

--- meat_shop/urls.py
+++ meat_shop/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from meat_shop import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', include('users.urls')),
+    path('users/', include('users.urls')),
     path('', include('products.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
