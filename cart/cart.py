875d621ed0004e09f825f48a07446fae6e97c596
Alash Shrestha <alashsth2019@gmail.com>
2024-03-28 09:58:00 +0545

    Add functions for cart

--- cart/cart.py
+++ cart/cart.py
@@ -1,19 +1,51 @@
+from products.models import Product
 
-class Cart():
+
+class Cart:
     def __init__(self, request):
         self.session = request.session
         # Get the current session key if exists
-        cart = self.session.get('session_key')
+        cart = self.session.get("session_key")
         # If the user is new, create a new session key
-        if 'session_key' not in request.session:
-            cart = self.session['session_key'] = {}
+        if "session_key" not in request.session:
+            cart = self.session["session_key"] = {}
         # Make cart available in all pages
         self.cart = cart
-        
-    def add(self, product):
+
+    def add(self, product, quantity):
         product_id = str(product.id)
+        product_qty = str(quantity)
         if product_id in self.cart:
             pass
         else:
-            self.cart[product_id] = {'price': str(product.price)}
+            # self.cart[product_id] = {"price": str(product.price)}
+            self.cart[product_id] = int(product_qty)
+        self.session.modified = True
+
+    def __len__(self):
+        return len(self.cart)
+
+    def get_prods(self):
+        # Get ids from cart
+        product_ids = self.cart.keys()
+        # Use ids to view products in database
+        products = Product.objects.filter(id__in=product_ids)
+        return products
+
+    def get_quants(self):
+        quantities = self.cart
+        return quantities
+
+    def update(self, product, quantity):
+        product_id = str(product)
+        product_qty = int(quantity)
+        # Update Cart
+        self.cart[product_id] = product_qty
+        self.session.modified = True
+        return self.cart
+
+    def delete(self, product):
+        product_id = str(product)
+        if product_id in self.cart:
+            del self.cart[product_id]
         self.session.modified = True
