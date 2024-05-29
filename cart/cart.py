47659add507a6589c9bdb93625c53fd616e2f5b2
Alash Shrestha <alashsth2019@gmail.com>
2024-05-29 18:18:06 +0545

    Update cart

--- cart/cart.py
+++ cart/cart.py
@@ -1,9 +1,11 @@
 from products.models import Product
+from users.models import Customer
 
 
 class Cart:
     def __init__(self, request):
         self.session = request.session
+        self.request = request
         # Get the current session key if exists
         cart = self.session.get("session_key")
         # If the user is new, create a new session key
@@ -11,6 +13,23 @@ class Cart:
             cart = self.session["session_key"] = {}
         # Make cart available in all pages
         self.cart = cart
+
+
+    def db_add(self, product, quantity):
+        product_id = str(product)
+        product_qty = str(quantity)
+        if product_id in self.cart:
+            pass
+        else:
+            # self.cart[product_id] = {"price": str(product.price)}
+            self.cart[product_id] = int(product_qty)
+        self.session.modified = True
+        if self.request.user.is_authenticated:
+            current_user = Customer.objects.filter(user=self.request.user)
+            carty = str(self.cart)
+            carty = carty.replace("\'", '\"')
+            current_user.update(old_cart=str(carty)) 
+
 
     def add(self, product, quantity):
         product_id = str(product.id)
@@ -21,6 +40,31 @@ class Cart:
             # self.cart[product_id] = {"price": str(product.price)}
             self.cart[product_id] = int(product_qty)
         self.session.modified = True
+        
+        if self.request.user.is_authenticated:
+            current_user = Customer.objects.filter(user=self.request.user)
+            carty = str(self.cart)
+            carty = carty.replace("\'", '\"')
+            current_user.update(old_cart=str(carty))
+
+    def cart_total(self):
+        product_ids = self.cart.keys()
+        # Search products with those keys in Product Database
+        products = Product.objects.filter(id__in=product_ids)
+        quantities = self.cart
+        total = 0
+        product_total = {}
+        for key, value in quantities.items():
+            key = int(key)
+            for product in products:
+                if product.id == key:
+                    if product.sale_price:
+                        product_total[key] = product.sale_price * value
+                        total += product.sale_price * value
+                    else:
+                        product_total[key] = product.price * value
+                        total += product.price * value
+        return {"total": total, "product_total": product_total}
 
     def __len__(self):
         return len(self.cart)
@@ -42,10 +86,23 @@ class Cart:
         # Update Cart
         self.cart[product_id] = product_qty
         self.session.modified = True
+        if self.request.user.is_authenticated:
+                current_user = Customer.objects.filter(user=self.request.user)
+                carty = str(self.cart)
+                carty = carty.replace("\'", '\"')
+                current_user.update(old_cart=str(carty))
         return self.cart
+    
+        
 
     def delete(self, product):
         product_id = str(product)
         if product_id in self.cart:
             del self.cart[product_id]
         self.session.modified = True
+        
+        if self.request.user.is_authenticated:
+            current_user = Customer.objects.filter(user=self.request.user)
+            carty = str(self.cart)
+            carty = carty.replace("\'", '\"')
+            current_user.update(old_cart=str(carty))
--- cart/views.py
+++ cart/views.py
@@ -2,25 +2,70 @@ from django.shortcuts import render, get_object_or_404
 from cart.cart import Cart
 from products.models import Product
 from django.http import JsonResponse
+from django.contrib import messages
+
 
 # Create your views here.
 def cart_view(request):
-    return render(request, 'cart/cart.html')
+    # Get the cart
+    if request.method == "POST":
+        searchData = request.POST.get("search_product")
+        if searchData != "":
+            products = Product.objects.filter(name__icontains=searchData)
+            if len(products) == 0:
+                messages.warning(request, "Products Not Found")
+            else:
+                return render(request, "products/index.html", {"products": products})
+    cart = Cart(request)
+    cart_products = cart.get_prods()
+    quantities = cart.get_quants()
+    cart_total = cart.cart_total()
+    total = cart_total["total"]
+    product_total = cart_total["product_total"]
+    return render(
+        request,
+        "cart/cart.html",
+        {"cart_products": cart_products, "quantities": quantities, "totals": total, "product_total": product_total},
+    )
+
 
 def cart_add(request):
     # Get the cart
     cart = Cart(request)
-    if request.POST.get('action') == 'post':
-        product_id = int(request.POST.get('product_id'))
+    if request.POST.get("action") == "post":
+        product_id = int(request.POST.get("product_id"))
+        product_qty = int(request.POST.get("product_qty"))
         # Look product in DB
         product = get_object_or_404(Product, id=product_id)
         # Save to session
-        cart.add(product=product)
-        response = JsonResponse({'Product Name ': product.name})
+        cart.add(product=product, quantity=product_qty)
+        # Get Cart quantity
+        cart_quantity = cart.__len__()
+        # response = JsonResponse({'Product Name ': product.name})
+        response = JsonResponse({"qty": cart_quantity})
+        messages.success(request, "Product Added to Cart")
         return response
 
+
 def cart_delete(request):
-    pass
+    # Get the cart
+    cart = Cart(request)
+    if request.POST.get("action") == "post":
+        product_id = int(request.POST.get("product_id"))
+        # Delete from session
+        cart.delete(product=product_id)
+        response = JsonResponse({"product": product_id})
+        messages.success(request, "Product Deleted from Cart")
+        return response
 
 def cart_update(request):
-    pass
+    # Get the cart
+    cart = Cart(request)
+    if request.POST.get("action") == "post":
+        product_id = int(request.POST.get("product_id"))
+        product_qty = int(request.POST.get("product_qty"))
+        # Update the session
+        cart.update(product=product_id, quantity=product_qty)
+        response = JsonResponse({"qty": product_qty})
+        messages.success(request, "Cart Updated")
+        return response
